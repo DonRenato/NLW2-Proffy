5cf8ff880ad18ff0b04d68ed93430b040d3573a9
Renato  Avellar <onizuka@Mac-mini.local>
2020-08-05 19:57:22 -0300

    integrando api com front

--- src/Pages/Landing/index.tsx
+++ src/Pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.svg';
 import landing from '../../assets/images/landing.svg';
@@ -7,8 +7,19 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import PurpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 function Landing(){
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(()=>{
+        api.get('/connections').then(res =>{
+            const { total } = res.data;
+
+            setTotalConnections(total)
+        })
+    },[]);
+
     return(
         <div id="page-landing">
             <div id="page-landing-content" className="container">
