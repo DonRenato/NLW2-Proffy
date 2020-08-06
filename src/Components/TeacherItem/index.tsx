603858cb65808d805c56a2b528b6de1c27eb4019
Renato  Avellar <onizuka@Mac-mini.local>
2020-08-05 23:58:06 -0300

    criando conexoes e finalizando o projeto web

--- src/Components/TeacherItem/index.tsx
+++ src/Components/TeacherItem/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {  
         id: number;
@@ -20,6 +21,13 @@ interface TeacherItemProps{
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection(){
+        api.post('/connections', {
+            user_id: teacher.id
+        });
+    }
+
+
     return(
         <article className="teacher-item">
             <header>
@@ -35,7 +43,10 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
                         Preço/Hora
                         <strong>R${teacher.cost},00</strong>
                     </p>
-                    <a href={`https://wa.me/${teacher.whatsapp}`}>
+                    <a 
+                    target="_blank" 
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}>
                         <img src={whatsAppIcon} alt="whatsApp" />
                         Entrar em contato
                     </a>
